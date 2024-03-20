e076a79ca3583adc8190d0679e9e9fee9b231dfd
Shubham Kumar <73250851+shubhamdev-code@users.noreply.github.com>
2024-03-21 02:12:42 +0530

    Update dotenvConfig.js

--- Backend/config/dotenvConfig.js
+++ Backend/config/dotenvConfig.js
@@ -1,7 +1,1 @@
 require('dotenv').config();
-console.log("dotenv loaded successfully");
-console.log(process.env.PORT);
-// module.exports = {
-//     PORT: process.env.PORT || 3000,
-//     MONGO_URI: process.env.MONGO_URI
-// };
